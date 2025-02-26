a5c2b873b91a7ec38aa47b161da43e4252c2db2a
mahshidsaleh <mahshids@stud.ntnu.no>
2025-02-26 13:32:15 +0100

    Coded die and dice classes

--- src/main/java/ntnu/idatt2003/Die.java
+++ src/main/java/ntnu/idatt2003/Die.java
@@ -1,6 +1,21 @@
 package ntnu.idatt2003;
 
-public class Die {
+import java.util.Random;
 
+/**
+ * Represents a single six-sided die.
+ */
+public class Die {
+  private Random random;
+
+  public Die() {
+    this.random = new Random();
+  }
+
+  /**
+   * Rolls the die and returns a number between 1 and 6.
+   * @return The rolled number.
+   */
+  public int roll() { return random.nextInt(6) + 1; }
 
 }
