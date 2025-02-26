d737620401ce08c9719a6a332577b25570525172
TheRiced <raisar@stud.ntnu.no>
2025-02-26 18:20:12 +0100

    add Tile with attributes and constructor

--- src/main/java/ntnu/idatt2003/Tile.java
+++ src/main/java/ntnu/idatt2003/Tile.java
@@ -1,5 +1,14 @@
 package ntnu.idatt2003;
 
 public class Tile {
+    private int tileId;
+    private TileAction landAction;
+
+    public Tile(int tileId) {
+        this.tileId = tileId;
+        
+    }
+
+    
 
 }
