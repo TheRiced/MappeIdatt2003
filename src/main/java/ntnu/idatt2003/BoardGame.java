32ed2803bd7d58d7c64350daf4b8336ea35af7ab
TheRiced <raisar@stud.ntnu.no>
2025-02-27 14:57:01 +0100

    create attributes and getter methods

--- src/main/java/ntnu/idatt2003/BoardGame.java
+++ src/main/java/ntnu/idatt2003/BoardGame.java
@@ -1,5 +1,39 @@
 package ntnu.idatt2003;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class BoardGame {
 
+    
+    private Board board;
+    private List<Player> players;
+    private Die die;
+    private int currentPlayerIndex;
+
+
+    public BoardGame(Board board, Die die, List<Player> players) {
+        this.board = board;
+        this.die = die;
+        this.players = players;
+        createBoard();
+    }
+
+    public void addPlayer(String name, int age) {
+        players.add(new Player(name, age));
+        Collections.sort(players);
+    }
+
+
+    public void createBoard() {
+        for (int i = 0; i < 90; i++) {
+            Tile tile = new Tile(i);
+            board.addTile(tile);
+        }
+    }
+
+    public int getBoardSize() {
+        return board.size();
+    }
 }
