b578276e7eb404e1d1ea448dab3983b63469b0dc
TheRiced <raisar@stud.ntnu.no>
2025-02-27 12:40:00 +0100

    Test class created with 50 reps

--- src/test/java/ntnu/idatt2003/DieTest.java
+++ src/test/java/ntnu/idatt2003/DieTest.java
@@ -1,7 +1,25 @@
 package ntnu.idatt2003;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
 
 class DieTest {
+    private Die die;
+
+    @BeforeEach
+    void setUp() {
+        die = new Die();
+    }
+
+    @Test
+    @RepeatedTest(50)
+    void testRoll() {
+        die.roll();
+        assertTrue(die.getValue() >= 1 && die.getValue() <= 6, "Die value is not between 1 and 6: " + die.getValue());
+    }
+
 
 }
