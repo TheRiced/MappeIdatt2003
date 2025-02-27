3e36512f23971feafbe15438df925408ec9707ea
TheRiced <raisar@stud.ntnu.no>
2025-02-27 14:55:55 +0100

    Implemetd addTile and getTile methods

--- src/main/java/ntnu/idatt2003/Board.java
+++ src/main/java/ntnu/idatt2003/Board.java
@@ -1,5 +1,22 @@
 package ntnu.idatt2003;
 
+import java.util.Map;
+
 public class Board {
+    //Add a map for tiles tiles: Map<Integer, Tile>
+    //Add a method to get the tile at a given position
+
+    //Methods:add tile, and getTile
+
+    private Map <Integer, Tile> tiles;  
+
+    public void addTile(Tile tile) {
+        tiles.put(tile.getTileId(), tile);
+    }
+
+    public Tile getTile(int tileId) {
+        return tiles.get(tileId);
+    }
+    
 
 }
