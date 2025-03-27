8b0c5a3d441cbf89c8a278006f6b12c92b381cc5
TheRiced <raisar@stud.ntnu.no>
2025-03-27 12:48:41 +0100

    feat(app): implement BoardGameApp with game setup and loop
    - Current only rolls for one player

--- src/main/java/ntnu/idatt2003/BoardGameApp.java
+++ src/main/java/ntnu/idatt2003/BoardGameApp.java
@@ -1,7 +1,60 @@
 package ntnu.idatt2003;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+import ntnu.idatt2003.model.Board;
+import ntnu.idatt2003.model.BoardGame;
+import ntnu.idatt2003.model.Player;
+import ntnu.idatt2003.model.Die;
+
+
+/**
+ *
+ */
 public class BoardGameApp {
-    
-    
-    
-}
+    public static void main(String[] args) {
+        Scanner ss = new Scanner(System.in);
+        Board board = new Board();
+        BoardGame game = new BoardGame(board, new ArrayList<Player>(), 2);
+
+        System.out.println("Welcome to the board game!");
+        System.out.println("Enter the number of players: ");
+        int numberOfPlayers = ss.nextInt();
+
+        if (numberOfPlayers < 2) {
+            System.out.println("You need at least 2 players to play the game.");
+            return;
+        }   
+
+        for (int i = 0; i < numberOfPlayers; i++) {
+            System.out.println("Enter the name of player " + (i + 1) + ": ");
+            String name = ss.next();
+            System.out.println("Enter the age of player " + (i + 1) + ": ");
+            int age = ss.nextInt();
+            game.addPlayer(name, age, game);
+        }
+
+
+        System.out.println("The game is ready to start!");
+        System.out.println("`\n Players in game: ");
+        for (Player player : game.getPlayers()) {
+            System.out.println(player.getName() + " (" + player.getAge() + ")");
+        }
+
+        int round = 1;
+        Die die = new Die();
+        while (!game.gameDone()) {
+            System.out.println("Round " + round);
+            for (Player player : game.getPlayers()) {
+                System.out.println(player.getName() + "'s turn");
+                System.out.println("Press enter to roll the dice");
+                ss.nextLine();
+                int roll = die.roll();
+                System.out.println(player.getName() + " rolled " + roll);
+                player.move(roll);
+                    break;
+                }
+            }
+            round++;
+        }
+    }
