03d48dcff3d3c185f690433a082bafb5d37d3880
mahshidsaleh <mahshids@stud.ntnu.no>
2025-04-09 19:12:15 +0200

    feat(Board): update Board class

--- src/main/java/ntnu/idatt2003/model/Board.java
+++ src/main/java/ntnu/idatt2003/model/Board.java
@@ -41,10 +41,10 @@ public class Board {
      */
     public void movePlayer(Player player, int steps) {
         Tile current = player.getCurrentTile();
-        Tile next = current;
+        Tile next = getTile(current.getNextTileId());
 
         // Steps forward through the tiles
-        for (int i = 0; i < steps; i++) {
+        for (int i = 1; i < steps; i++) {
             if (next.getNextTileId() != 0) {
                 next = getTile(next.getNextTileId());
             } else {
