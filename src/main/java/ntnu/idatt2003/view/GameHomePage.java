ae3733c6bb8b5b6a635b1e7f2e1cf454a2f20712
TheRiced <raisar@stud.ntnu.no>
2025-04-10 10:47:23 +0200

    feat: add method to hover titles

--- src/main/java/ntnu/idatt2003/view/GameHomePage.java
+++ src/main/java/ntnu/idatt2003/view/GameHomePage.java
@@ -1,10 +1,14 @@
 package ntnu.idatt2003.view;
 
+import javafx.scene.control.Label;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 
@@ -23,6 +27,30 @@ public class GameHomePage extends Application {
     stage.setScene(scene);
     stage.setTitle("Game Hub");
     stage.show();
+  }
+
+
+  private Label createHoverLabel(String text, Color glowColor) {
+    Label label = new Label(text);
+    label.setTextFill(Color.DARKGRAY);
+    label.setFont(Font.font("Segoe UI", 40));
+
+    DropShadow glow = new DropShadow(30, glowColor);
+    glow.setSpread(0.5);
+
+    label.setOnMouseEntered(e -> {
+      label.setEffect(glow);
+      label.setScaleX(1.1);
+      label.setScaleY(1.1);
+    });
+
+    label.setOnMouseExited(e -> {
+      label.setEffect(null);
+      label.setScaleX(1.0);
+      label.setScaleY(1.0);
+    });
+
+    return label;
   }
 
   public static void main(String[] args) {
