b1d53f30b1e5696d1cd20b31a501939d51c87cbe
TheRiced <raisar@stud.ntnu.no>
2025-04-10 10:45:14 +0200

    feat: add center Vbox for game content

--- src/main/java/ntnu/idatt2003/view/GameHomePage.java
+++ src/main/java/ntnu/idatt2003/view/GameHomePage.java
@@ -1,16 +1,27 @@
 package ntnu.idatt2003.view;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
 
 public class GameHomePage extends Application {
 
   @Override
   public void start(Stage stage) {
-    Scene scene = new Scene(new javafx.scene.layout.StackPane(), 800, 600);
-    stage.setTitle("Game Hub");
+    StackPane root = new StackPane();
+    root.setStyle("-fx-background-color: linear-gradient(to bottom right, #111111, #000000);");
+
+    VBox content = new VBox(40);
+    content.setAlignment(Pos.CENTER);
+    root.getChildren().add(content);
+
+    Scene scene = new Scene(root, 800, 600);
     stage.setScene(scene);
+    stage.setTitle("Game Hub");
     stage.show();
   }
 
