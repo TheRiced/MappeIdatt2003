e3966edcce5926fc94d432391636216e1dc2e314
mahshidsaleh <mahshids@stud.ntnu.no>
2025-04-28 11:01:36 +0200

    feat(observer): add methods in Observer class

--- src/main/java/ntnu/idatt2003/view/Observer.java
+++ src/main/java/ntnu/idatt2003/view/Observer.java
@@ -1,6 +1,10 @@
 package ntnu.idatt2003.view;
 
+import ntnu.idatt2003.model.Player;
+
 public interface Observer {
-  void update();
+  void onPlayerMoved(Player player, int fromTileId, int toTileId);
+  void onNextPlayer(Player next);
+  void onGameOver(Player winner);
 
 }
