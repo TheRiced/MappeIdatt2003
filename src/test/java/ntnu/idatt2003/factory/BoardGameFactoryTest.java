cb5650b8af29a469dc968ef66ef51d84ada5d820
TheRiced <raisar@stud.ntnu.no>
2025-05-01 01:10:28 +0200

    Add BoardGameFactoryTest with stub methods

---
+++ src/test/java/ntnu/idatt2003/factory/BoardGameFactoryTest.java
@@ -0,0 +1,20 @@
+package ntnu.idatt2003.factory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BoardGameFactoryTest {
+
+  @Test
+  void createSnakeAndLadderGameFromFile() {
+  }
+
+  @Test
+  void createGameFromFile() {
+  }
+
+  @Test
+  void createDefaultSnakeAndLadderGame() {
+  }
+}
