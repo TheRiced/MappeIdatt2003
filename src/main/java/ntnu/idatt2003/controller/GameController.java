1830023d6b2b442b71862cc4c89a4a1c648a9201
TheRiced <raisar@stud.ntnu.no>
2025-05-08 15:38:09 +0200

    refactor(controller): integrate Animator & support animated moves with snakes/ladders
    - Instantiate and inject new `Animator` into `BoardView`
    - Update `GameController` constructor to pass `Animator` when creating `BoardView`

--- src/main/java/ntnu/idatt2003/controller/GameController.java
+++ src/main/java/ntnu/idatt2003/controller/GameController.java
@@ -10,6 +10,7 @@ import ntnu.idatt2003.factory.BoardGameFactory;
 import ntnu.idatt2003.model.BoardGame;
 import ntnu.idatt2003.model.Player;
 import ntnu.idatt2003.model.SnakeAndLadderGame;
+import ntnu.idatt2003.view.Animator;
 import ntnu.idatt2003.view.BoardView;
 
 public class GameController {
@@ -18,29 +19,36 @@ public class GameController {
   private final BoardView boardView;
   private final BoardGame game;
 
-  public GameController(Stage stage, Path boardJson, List<Player> players, int diceCount) throws
-      Exception {
-    this.stage = stage;
 
+
+  public GameController(Stage stage, Path boardJson, List<Player> players, int diceCount) throws Exception {
+    this.stage = stage;
 
     BoardGameFactory factory = new BoardGameFactory();
     this.game = factory.createGameFromFile(boardJson, players, diceCount);
-    //factory.createSnakeAndLadderGameFromFile(boardJson,
-    //players, diceCount);
-    this.boardView = new BoardView(game.getBoard(), game.getPlayers());
+
+    // 1) Create the Animator
+    Animator animator = new Animator();
+
+    // 2) Inject it into your BoardView
+    this.boardView = new BoardView(game.getBoard(), game.getPlayers(), animator);
 
     boardView.drawActions();
-
     game.addObserver(boardView);
 
-
-    this.boardView.getRollDiceButton().setOnAction(e -> handleRoll());
+    boardView.getRollDiceButton().setOnAction(e -> handleRoll());
   }
+
 
   public void start() {
     Scene scene = new Scene(boardView, 900, 600);
     stage.setScene(scene);
     stage.show();
+
+
+
+    // 3) Optionally, start the drift for all players immediately:
+    game.getPlayers().forEach(boardView::startPlayerDrift);
 
     boardView.updateCurrentPlayer(game.getCurrentPlayer().getName());
 
@@ -50,25 +58,34 @@ public class GameController {
     Player current = game.getCurrentPlayer();
     int fromId = current.getCurrentTile().getTileId();
 
+    // 1) Roll dice
     List<Integer> lastRoll = game.rollIndividual();
     int rolled = lastRoll.stream().mapToInt(Integer::intValue).sum();
     boardView.updateDiceResult(rolled);
+
+    // 2) Figure out the “mid” tile before any snake/ladder
+    int midId = fromId + rolled;
+
+    // 3) Move your model (this will also apply snake/ladder internally,
+    //    but we’ll animate that separately)
     game.moveCurrentPlayer(rolled);
-    boardView.movePlayer(current, fromId);
 
-    int toId = current.getCurrentTile().getTileId();
-    boardView.movePlayer(current, fromId);
+    // 4) Animate the straight walk to midId
+    boardView.animatePlayerMove(current, fromId, midId, () -> {
+      // 5) Once that finishes, let the view check for a snake/ladder
+      boardView.finishActionJump(current, midId);
 
-    if (game.gameDone()) {
-      boardView.showWinner(game.getWinner().getName());
-      boardView.getRollDiceButton().setDisable(true);
-      return;
-    }
-    if (!game.playerGetsExtraTurn(lastRoll)) {
-      game.nextPlayer();
-    }
-
-    boardView.updateCurrentPlayer(game.getCurrentPlayer().getName());
+      // 6) Then do turn-end logic:
+      if (game.gameDone()) {
+        boardView.showWinner(game.getWinner().getName());
+        boardView.getRollDiceButton().setDisable(true);
+      } else {
+        if (!game.playerGetsExtraTurn(lastRoll)) game.nextPlayer();
+        boardView.updateCurrentPlayer(game.getCurrentPlayer().getName());
+      }
+    });
   }
+
+
 
 }
