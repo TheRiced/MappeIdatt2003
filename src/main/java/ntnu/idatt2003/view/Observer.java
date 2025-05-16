e5ec1dc1f664ef8f04d916c53bae1a5563d6d160
TheRiced <raisar@stud.ntnu.no>
2025-05-16 13:26:05 +0200

    feat(view): add onDiceRolled to Observer interface for detailed dice tracking

--- src/main/java/ntnu/idatt2003/view/Observer.java
+++ src/main/java/ntnu/idatt2003/view/Observer.java
@@ -1,10 +1,14 @@
 package ntnu.idatt2003.view;
 
+import java.util.List;
 import ntnu.idatt2003.model.Player;
 
 public interface Observer {
 
   void onPlayerMoved(Player player, int fromTileId, int toTileId);
+
+  void onDiceRolled(List<Integer> values);
+
   void onNextPlayer(Player next);
   void onGameOver(Player winner);
 
