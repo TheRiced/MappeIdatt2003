f900adede82cad96fcb481e742f88a3fc5daaff9
TheRiced <raisar@stud.ntnu.no>
2025-05-22 20:29:03 +0200

    added removeObserver again

--- src/main/java/ntnu/idatt2003/model/BoardGame.java
+++ src/main/java/ntnu/idatt2003/model/BoardGame.java
@@ -2,6 +2,7 @@ package ntnu.idatt2003.model;
 
 import java.util.List;
 import ntnu.idatt2003.model.ludo.Token;
+import ntnu.idatt2003.model.snakeandladder.SnakeLadderPlayer;
 import ntnu.idatt2003.view.Observer;
 
 /**
@@ -19,5 +20,8 @@ public interface BoardGame<P, B> {
   boolean gameDone();
   P getWinner();
   void addObserver(Observer<P> observer);
+
+  void removeObserver(Observer<SnakeLadderPlayer> observer);
+
   void selectToken(Token token);
 }
